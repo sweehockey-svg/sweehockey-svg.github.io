e2bb730a2ed9227e86a86971748dd01475ead438
sweehockey-svg <sweehockey@gmail.com>
2026-06-03 09:12:52 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+﻿window.SEC_CONFIG = {
+  sheetUrl: "./database-cups-extra.json",
+  rulesUrl: "",
+  placementsUrl: "",
+  rawDataApiBaseUrl: "",
+  dnfTeamsByCup: {
+    "SEC 20 DIV 2": ["Dynamite sharks"]
+  },
+  cupSettingsOverrides: {
+    "SEC 14": {
+      playoffCut1: 3,
+      playoffCut2: 5
+    }
+  },
+  databaseUrl: "./database-cups.json",
+  teamLogoBaseUrl: "https://sweehockey-svg.github.io/teamlogos",
+  playerImageBaseUrl: "https://sweehockey-svg.github.io/players",
+  teamLogoManifestUrl: "https://api.github.com/repos/sweehockey-svg/sweehockey-svg.github.io/contents/teamlogos",
+  playerImageManifestUrl: "https://api.github.com/repos/sweehockey-svg/sweehockey-svg.github.io/contents/players",
+  signupApiUrl: "https://script.google.com/macros/s/AKfycbxhNNjRiz7OcrT2zDrGgj_h2GCkkWYOjDo7oq4xRhXH6socZXc6dmJFpZHPRuTpnUoI/exec"
+};
