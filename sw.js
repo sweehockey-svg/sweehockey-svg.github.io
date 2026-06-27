0b3f7bc88b3f2e0c5d9d959c716b388594ab9f21
sweehockey-svg <sweehockey@gmail.com>
2026-06-27 15:21:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = "sw-image-rescue-20260627j";
+const ASSET_VERSION = "sw-image-rescue-20260627k";
 const MANIFESTS = {
   players: { file: "players.json", folder: "players", items: null, promise: null },
   teamlogos: { file: "teamlogos.json", folder: "teamlogos", items: null, promise: null }
