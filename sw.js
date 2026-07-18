2bf027e522968e4270440b0090aa74a70ada9c8b
sweehockey-svg <sweehockey@gmail.com>
2026-07-18 12:41:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = "v5-blank-20260718ai";
+const ASSET_VERSION = "v5-blank-20260718aj";
 const JSON_CACHE = "svensk-ehockey-json";
 const MANIFESTS = {
   teamlogos: { file: "teamlogos.json", folder: "teamlogos", items: null, promise: null }
