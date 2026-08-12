a2cd074dda84191fcd240df2d2c211bd7a4544ca
sweehockey-svg <sweehockey@gmail.com>
2026-08-12 18:19:48 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+/*
+  Klistra in Project URL och PUBLISHABLE KEY från Supabase.
+
+  Använd aldrig:
+  - Secret key
+  - service_role key
+  - databaslösenord
+*/
+
+window.EHOCKEY_CONFIG = {
+  supabaseUrl: "https://oujqnvrczdavqbqaavuh.supabase.co",
+  supabasePublishableKey: "sb_publishable_-4cV-I1xCAAZrgdcGCljrQ_T7T0YC5z"
+};
