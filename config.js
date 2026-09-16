6ad7ccb64bbc492b6cfff180ef3d8f7d5fb836e6
sweehockey-svg <sweehockey@gmail.com>
2026-09-17 01:15:09 +0200

    Bust admin pending badge cache

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ window.EHOCKEY_CONFIG = {
   document.head.appendChild(css);
 
   const script = document.createElement('script');
-  script.src = '/header-admin-pending-v1.js?v=20260917-1';
+  script.src = '/header-admin-pending-v1.js?v=20260917-2';
   script.defer = true;
   document.head.appendChild(script);
 })();
