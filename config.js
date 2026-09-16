bef9a88956b399ec9d7bc9759eaf5cbc33e1bf37
sweehockey-svg <sweehockey@gmail.com>
2026-09-17 01:10:55 +0200

    Load compact admin pending header badge

--- config.js
+++ config.js
@@ -11,3 +11,16 @@ window.EHOCKEY_CONFIG = {
   supabaseUrl: "https://oujqnvrczdavqbqaavuh.supabase.co",
   supabasePublishableKey: "sb_publishable_-4cV-I1xCAAZrgdcGCljrQ_T7T0YC5z"
 };
+
+/* Huvudmenyn: adminverktygen ligger i kontodropdownen. Visa endast väntande ärenden. */
+(() => {
+  const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = '/header-admin-pending-v1.css?v=20260917-1';
+  document.head.appendChild(css);
+
+  const script = document.createElement('script');
+  script.src = '/header-admin-pending-v1.js?v=20260917-1';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
