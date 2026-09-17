9faf49f9d71ef9cd5cb12ecfc0b60d50a3f3fc10
sweehockey-svg <sweehockey@gmail.com>
2026-09-17 22:12:02 +0200

    Coordinate Mitt eHockey personal modules

--- config.js
+++ config.js
@@ -186,3 +186,11 @@ window.EHOCKEY_CONFIG = {
   script.defer = true;
   document.head.appendChild(script);
 })();
+
+/* Mitt eHockey: håll personliga tilläggsmoduler monterade även vid sen webbapp-init eller omrendering. */
+(() => {
+  const script = document.createElement('script');
+  script.src = '/webapp-my-ehockey-modules-sync-v1.js?v=20260917-1';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
