188a727c5c9c3df6f678d505a6ebeb0d7a51ac5c
sweehockey-svg <sweehockey@gmail.com>
2026-09-17 18:42:44 +0200

    Bump Mitt eHockey career script cache

--- config.js
+++ config.js
@@ -130,7 +130,7 @@ window.EHOCKEY_CONFIG = {
   document.head.appendChild(css);
 
   const script = document.createElement('script');
-  script.src = '/webapp-my-ehockey-v1.js?v=20260917-career-1';
+  script.src = '/webapp-my-ehockey-v1.js?v=20260917-career-2';
   script.defer = true;
   document.head.appendChild(script);
 })();
