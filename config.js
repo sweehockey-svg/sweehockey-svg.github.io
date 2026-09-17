8667b8fc6af761ffc9db0afa76eeb496f2f6962a
sweehockey-svg <sweehockey@gmail.com>
2026-09-17 05:30:45 +0200

    Bust personalized home cache after team label fix

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ window.EHOCKEY_CONFIG = {
   document.head.appendChild(css);
 
   const script = document.createElement('script');
-  script.src = '/webapp-for-you-v1.js?v=20260917-phase-aware-1';
+  script.src = '/webapp-for-you-v1.js?v=20260917-current-team-only-1';
   script.defer = true;
   document.head.appendChild(script);
 })();
